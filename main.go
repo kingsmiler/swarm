f1cf8af5dd12166a5c66e67b093fe8585a936dff
kingsmiler <kingsmiler@163.com>
2016-01-16 00:16:23 +0800

    主函数说明

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+// 通过使用下划线_别名, 仅执行这些包下面所有文件的init方法,
+// 并不会真正的导入这些包.
 	_ "github.com/docker/docker/pkg/discovery/file"
 	_ "github.com/docker/docker/pkg/discovery/kv"
 	_ "github.com/docker/docker/pkg/discovery/nodes"
